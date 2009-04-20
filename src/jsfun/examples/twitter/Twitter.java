efa965839ee2083190f6b89575112777fa1881ab
Charles Lowell <cowboyd@thefrontside.net>
2009-04-20 00:22:17 -0400

    add a scriptable twitter interface accessible by a little jetty server

--- src/jsfun/examples/twitter/Twitter.java
+++ src/jsfun/examples/twitter/Twitter.java
@@ -2,21 +2,57 @@ package jsfun.examples.twitter;
 
 import jsfun.utils.JSEnvironment;
 import jsfun.utils.CleanScope;
-import org.mozilla.javascript.Scriptable;
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Script;
-import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.*;
 
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.InvocationTargetException;
 
 
-public class Twitter implements JSEnvironment {
+public class Twitter implements JSEnvironment, ClassShutter {
 	@Override
 	public Scriptable createScope(Context cx) throws Exception {
 		Script script = cx.compileReader(new InputStreamReader(Twitter.class.getResourceAsStream("twitter.js")), "twitter.js", 1, null);
 		ScriptableObject scope = cx.initStandardObjects();
-		script.exec(cx, scope);
+		Scriptable api = (Scriptable) script.exec(cx, scope);
+		for (Object id : api.getIds()) {
+			String name = (String)id;
+			Scriptable function = (Scriptable) api.get(name, api);
+			scope.put(name, scope, function);
+		}
+		setupContext(cx);
+		//cx.setClassShutter(this);
 		CleanScope.clean(scope);
 		return scope;
+	}
+
+	public static void setupContext(Context cx) {
+		cx.putThreadLocal("TWITTER_API", newApiImpl());
+	}
+
+	@Override
+	public boolean visibleToScripts(String fullClassName) {
+		return fullClassName.equals("$Proxy0");
+	}
+
+	public static class ApiInvoker implements InvocationHandler {
+
+		private ApiImpl impl = new ApiImpl();
+
+		@Override
+		public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+			try {
+				return method.invoke(impl, objects);
+			} catch (InvocationTargetException e) {
+				throw e.getCause();
+			}
+		}
+
+	}
+
+	private static Object newApiImpl() {
+		return Proxy.newProxyInstance(Twitter.class.getClassLoader(), new Class[] {Api.class}, new ApiInvoker());
 	}
 }
--- src/jsfun/examples/twitter/twitter.js
+++ src/jsfun/examples/twitter/twitter.js
@@ -1,78 +1,123 @@
+((function() {
 
-
-
-(function() {
-	var twitter = new Packages.twitter4j.Twitter()
+	var Context = Packages.org.mozilla.javascript.Context;
 	var Query = Packages.twitter4j.Query
+	//var out = java.lang.System.out
 
-	var FIELDS = {
-		Status: ['createdAt', 'id', 'inReplyToStatusId', 'inReplyToUserId', 'source', 'text', 'user'],
-		User: ['description', 'followersCount', 'id', 'location', 'name', 'profileImageURL', 'screenName'],
-		QueryResult: ['page', 'query', 'total', 'tweets'],
-		Tweet: ['id', 'createdAt', 'fromUser', 'toUser', 'source', 'text']		
+
+	function string(s) {
+		return "" + s
 	}
 
-	function unwrap(object) {
-		var fields = object.getClass ? FIELDS[object.getClass().getSimpleName()] : null
-		if (fields) {
-			var value = {}
-			for (var field in fields) {
-				value[field] = unwrap(object[field])
+	function List(transform) {
+		return function(list) {
+			var mapped = []
+			for (var i = list.iterator(); i.hasNext();) {
+				var item = i.next();
+				mapped.push(transform(item))
 			}
-			return value
-		} else if (object) {
-			return "" + object.toString()
+			return mapped
+		}
+	}
+
+	function User(user) {
+		return {
+			id: user.id,
+			name: string(user.name),
+			screenName: string(user.screenName),
+			description: string(user.description),
+			followersCount: user.followersCount,
+			location: string(user.location),
+			statusText: string(user.statusText)
+		}
+	}
+
+	function Status(status) {
+		return {
+			id: status.id,
+			inReplyToStatusId: status.inReplyToStatusId,
+			source: string(status.source),
+			text: string(status.text),
+			user: User(status.user)
+		}
+	}
+
+	function QueryResult(result) {
+		return {
+			completedIn: result.completedIn,
+			maxId: result.maxId,
+			page: result.page,
+			query: string(result.query),
+			warning: string(result.warning),
+			tweets: List(Tweet)(result.tweets)
+		}
+	}
+
+	function Tweet(t) {
+		return {
+			id: t.id,
+			fromUser: string(t.fromUser),
+			fromUserId: t.fromUserId,
+			toUser: string(t.toUser),
+			toUserId: t.toUserId,
+			isoLanguageCode: string(t.isoLanguageCode),
+			text: string(t.text),
+			source: string(t.source)
+		}
+	}
+
+	function apicall(name, arguments, transform) {
+		var api = Context.getCurrentContext().getThreadLocal("TWITTER_API")
+		var result = api[name].apply(api, arguments);
+		if (transform) {
+			return transform(result)
 		} else {
-			return ""
+			return new Object().undefined
 		}
 	}
 
-	function method(impl) {
-		var fun = function() {
-			return unwrap(impl.apply(this, arguments))
+//	function puts(str) {
+//		out.println('' + str)
+//	}
+
+	return {
+		login: function(username, password) {
+			apicall('login', [username, password])
+		},
+		friends: function() {
+			return apicall('getFriends', arguments, List(User))
+		},
+		followers: function() {
+			return apicall('getFollowers', arguments, List(User))
+		},
+		timeline: function() {
+			return apicall('getFriendsTimeline', arguments, List(Status))
+		},
+		search: function(query) {
+			query = query || {}
+			if (typeof query == 'string') {
+				query = {
+					query: query
+				}
+			}
+			var q = new Query()
+			var fields = ['lang', 'page', 'query', 'rpp', 'sinceId']
+			for (var i = 0; i < fields.length; i++) {
+				var field = fields[i]
+				if (typeof query[field] != 'undefined') {
+					q[field] = query[field]
+				}
+			}
+			if (query.geocode) {
+				var els = query.geocode.toString().split(/,/)
+				if (!els[1]) {
+					els[1] = els[2] = null
+				} else if (!el[2]) {
+					els[2] = null
+				}
+				q.setGeocode(els[0], els[1], els[2])
+			}
+			return apicall('search', [q], QueryResult)
 		}
-		fun.toSource = fun.toString = function() {return "function() { [Native Code] }"}
 	}
-
-	login = method(function(username, password) {
-		twitter.userId = username
-		twitter.password = password
-	})
-
-	friends = method(function() {
-		return twitter.getFriends.apply(twitter, arguments)
-	})
-
-	followers = method(function() {
-		return twitter.getFollowers.apply(twitter, arguments)
-	})
-
-	timeline = method(function() {
-		return twitter.getFriendsTimeline.apply(twitter, arguments)
-	})
-
-	search = function(query) {
-		query = query || {}
-		if (typeof query == 'string') {
-			query = {
-				query: query
-			}
-		}
-		var q = new Query()
-		for (var field in ['lang', 'page', 'query', 'rpp', 'sinceId']) {
-			if (query[field]) {
-				q[field] = query[field]
-			}
-		}
-		if (query.geocode) {
-			var els = query.geocode.toString().split(/,/)
-			if (!els[1]) {
-				els[1] = els[2] = null
-			} else if (!el[2]) {
-				els[2] = null
-			}
-			q.setGeocode(els[0],els[1], els[2])
-		}
-		return twitter.search(q)
-	}
-})()
+})())
