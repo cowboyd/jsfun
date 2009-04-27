ece033c504099537d3a31f74e85df5c35fcc646a
Charles Lowell <cowboyd@thefrontside.net>
2009-04-27 02:10:10 -0400

    added new slides and designs to demonstrate scriptable twitter webservice

--- src/jsfun/examples/twitter/server/TwitterScriptServlet.java
+++ src/jsfun/examples/twitter/server/TwitterScriptServlet.java
@@ -104,10 +104,6 @@ public class TwitterScriptServlet extends HttpServlet implements ContextAction {
 		@Override
 		protected void onContextCreated(Context cx) {
 			cx.setInstructionObserverThreshold(100 * 1000);
-			Api api = Twitter.setupContext(cx);
-			String username = request.getParameter("u");
-			String password = request.getParameter("p");
-			api.login(username, password);
 			super.onContextCreated(cx);
 		}
 
